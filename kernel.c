f7caa4497d1358a064273d403d1de88bb2a32805
7078-cj <7078ceejay@gmail.com>
2026-08-23 07:33:07 +0800

    refactor printf function to improve cursor handling and memory writing

--- kernel.c
+++ kernel.c
@@ -1,15 +1,30 @@
 #include "types.h"
 
+uint16_t* VideoMemory = (uint16_t*)0xb8000;
+
+uint8_t CursorX = 0;
+uint8_t CursorY = 0;
+
 void printf(char* str){
 
-    uint16_t* VideoMemory = (uint16_t*)0xb8000;
-
     for(int i = 0; str[i] != '\0'; i++){
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+
+        if (str[i] == '\n') {
+            CursorX = 0;
+            CursorY++;
+            continue;
+        }
+
+        int offset = CursorY * 80 + CursorX;
+        VideoMemory[offset] = (VideoMemory[offset] & 0xFF00) | str[i];
+        CursorX++;
+
+        if (CursorX >= 80) {
+            CursorX = 0;
+            CursorY++;
+        }
     }
 }
-
-typedef void (*constructor)(void);
 
 typedef void (*constructor)(void);
 
@@ -23,6 +38,7 @@ void callConstructors(){
 }
 void kernelMain(void* multiboot_structure, unsigned int magic_number){
     printf("Hello World WOW \n");
-
+    printf("Wrahhhhh \n");
+    
     while(1);
 }
