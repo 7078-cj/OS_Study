f43eb79258b61adea37e761e660629cb8c2099a1
7078-cj <7078ceejay@gmail.com>
2026-08-29 17:02:04 +0800

    refactor: update Port and Port8Bit structures with new function declarations

--- port/port.h
+++ port/port.h
@@ -1,3 +1,4 @@
+
 #ifndef PORT_H
 #define PORT_H
 
@@ -5,7 +6,7 @@
 
 
 /* ------------------------------------------------------------------ */
-/* Port : base type                                                    */
+/* Port : base type                                                   */
 /* ------------------------------------------------------------------ */
 
 typedef struct Port {
@@ -17,7 +18,7 @@ void Port_deinit(Port* self);
 
 
 /* ------------------------------------------------------------------ */
-/* Port8Bit : public Port                                              */
+/* Port8Bit : public Port                                             */
 /* ------------------------------------------------------------------ */
 
 typedef struct Port8Bit Port8Bit;
@@ -34,26 +35,29 @@ struct Port8Bit {
 };
 
 void Port8Bit_init(Port8Bit* self, uint16_t portnumber);
+void Port8Bit_Write(Port8Bit* self, uint8_t data);
+uint8_t Port8Bit_Read(Port8Bit* self);
 void Port8Bit_destroy(Port8Bit* self);
 
 
 /* ------------------------------------------------------------------ */
-/* Port8BitSlow : public Port8Bit                                      */
+/* Port8BitSlow : public Port8Bit                                     */
 /* ------------------------------------------------------------------ */
 
 typedef struct Port8BitSlow Port8BitSlow;
 
-typedef struct Port8BitSlow {
+struct Port8BitSlow {
     Port8Bit base;
     void (*Write)(Port8BitSlow* self, uint8_t data);
-} Port8BitSlow;
+};
 
 void Port8BitSlow_init(Port8BitSlow* self, uint16_t portnumber);
+void Port8BitSlow_Write(Port8BitSlow* self, uint8_t data);
 void Port8BitSlow_destroy(Port8BitSlow* self);
 
 
 /* ------------------------------------------------------------------ */
-/* Port16Bit : public Port                                             */
+/* Port16Bit : public Port                                          */
 /* ------------------------------------------------------------------ */
 
 typedef struct Port16Bit Port16Bit;
@@ -70,7 +74,10 @@ struct Port16Bit {
 };
 
 void Port16Bit_init(Port16Bit* self, uint16_t portnumber);
+void Port16Bit_Write(Port16Bit* self, uint16_t data);
+uint16_t Port16Bit_Read(Port16Bit* self);
 void Port16Bit_destroy(Port16Bit* self);
+
 
 /* ------------------------------------------------------------------ */
 /* Port32Bit : public Port                                             */
@@ -96,3 +103,4 @@ void Port32Bit_destroy(Port32Bit* self);
 
 
 #endif /* PORT_H */
+
